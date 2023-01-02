6f1538fab6a570a9fd0c585754f08625d747d1db
Aldi L William <aldyy1988@gmail.com>
2023-01-02 10:45:25 +0700

    Update IP Whitelist dengan x forwarded for

--- middlewares/whitelistIP.go
+++ middlewares/whitelistIP.go
@@ -1,17 +1,28 @@
 package middlewares
 
 import (
+	"net"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func IPWhiteList(whitelist map[string]bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if !whitelist[c.ClientIP()] {
+		var ip string
+		forwardHeader := c.Request.Header.Get("x-forwarded-for")
+		firstAddress := strings.Split(forwardHeader, ",")[0]
+		if net.ParseIP(strings.TrimSpace(firstAddress)) != nil {
+			ip = firstAddress
+		} else {
+			ip = c.ClientIP()
+		}
+
+		if !whitelist[ip] {
 			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
 				"status":  http.StatusForbidden,
-				"message": c.ClientIP(),
+				"message": ip,
 			})
 			return
 		}
--- server/cors.go
+++ server/cors.go
@@ -22,7 +22,7 @@ func AllowCors(environment string) {
 
 	config.AllowWildcard = true
 	config.AllowOrigins = whitelists
-	config.AllowHeaders = append(config.AllowHeaders, "Authorization", "Accept-Encoding", "X-Digiflazz-Event", "X-Digiflazz-Delivery", "X-Hub-Signature", "Content-Type")
+	config.AllowHeaders = append(config.AllowHeaders, "Authorization", "Accept-Encoding", "X-Digiflazz-Event", "X-Digiflazz-Delivery", "X-Hub-Signature", "Content-Type", "X-Forwarded-For")
 
 	router.Use(cors.New(config))
 }
