6df436c8035bc0ff4a804adb1a6f469f40d77bce
Aldi L William <aldyy1988@gmail.com>
2023-01-02 10:59:02 +0700

    Update WhitelistIP Address

--- middlewares/whitelistIP.go
+++ middlewares/whitelistIP.go
@@ -1,28 +1,17 @@
 package middlewares
 
 import (
-	"net"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func IPWhiteList(whitelist map[string]bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var ip string
-		forwardHeader := c.Request.Header.Get("x-forwarded-for")
-		firstAddress := strings.Split(forwardHeader, ",")[0]
-		if net.ParseIP(strings.TrimSpace(firstAddress)) != nil {
-			ip = firstAddress
-		} else {
-			ip = c.ClientIP()
-		}
-
-		if !whitelist[ip] {
+		if !whitelist[c.ClientIP()] {
 			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
 				"status":  http.StatusForbidden,
-				"message": ip,
+				"message": c.ClientIP(),
 			})
 			return
 		}
